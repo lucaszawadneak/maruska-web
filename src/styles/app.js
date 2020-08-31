d2c9a00282137ba018a2a5b6704e404269c31c76
Lucas <lucaszawadneak@gmail.com>
2020-08-31 10:01:04 -0300

    Initial commit1

---
+++ src/styles/app.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Background = styled.image`
+  height: 100%;
+  width: 100%;
+`;
